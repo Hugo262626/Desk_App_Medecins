b5050a7520a91cd34666814d7d783502cd4c31a1
hugob <hugob@LAPTOP-V6M8LCR8>
2026-02-03 08:49:14 +0100

    context update

--- rattrapageB4/ClinicContext.cs
+++ rattrapageB4/ClinicContext.cs
@@ -1,27 +1,26 @@
-﻿using System.Collections.Generic;
-using System.Reflection.Emit;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using rattrapageB4.Models;
 
 namespace rattrapageB4
 {
     public class ClinicContext : DbContext
     {
-        public DbSet<Speciality> Specialities { get; set; }
-        public DbSet<Doctor> Doctors { get; set; }
-        public DbSet<Patient> Patients { get; set; }
-        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Speciality> Specialities { get; set; } = null!;
+        public DbSet<Doctor> Doctors { get; set; } = null!;
+        public DbSet<Patient> Patients { get; set; } = null!;
+        public DbSet<Appointment> Appointments { get; set; } = null!;
 
-        // Configurer la base ici
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            string connectionString = "Server=localhost;Database=clinic;User=root;Password=mysql;";
-            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            if (!optionsBuilder.IsConfigured)
+            {
+                string connectionString = "Server=localhost;Database=clinic;User=root;Password=mysql;";
+                optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
+            }
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // Empêcher un médecin d’avoir des RDV en conflit
-            // (contrainte de validation dans EF, on fera une vérif plus tard côté service)
             modelBuilder.Entity<Appointment>()
                 .HasOne(a => a.Doctor)
                 .WithMany(d => d.Appointments)
@@ -34,55 +33,14 @@ namespace rattrapageB4
                 .HasForeignKey(a => a.PatientId)
                 .OnDelete(DeleteBehavior.Cascade);
 
+            // Indices utiles (performance agenda + conflits)
+            modelBuilder.Entity<Appointment>()
+                .HasIndex(a => new { a.DoctorId, a.StartAt });
+
+            modelBuilder.Entity<Appointment>()
+                .HasIndex(a => new { a.PatientId, a.StartAt });
+
             base.OnModelCreating(modelBuilder);
         }
-    }
-
-    // ---- MODELES ----
-
-    public class Speciality
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-
-        public List<Doctor> Doctors { get; set; }
-    }
-
-    public class Doctor
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-
-        public int SpecialityId { get; set; }
-        public Speciality Speciality { get; set; }
-
-        public List<Appointment> Appointments { get; set; }
-    }
-
-    public class Patient
-    {
-        public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
-
-        public List<Appointment> Appointments { get; set; }
-    }
-
-    public class Appointment
-    {
-        public int Id { get; set; }
-
-        public int PatientId { get; set; }
-        public Patient Patient { get; set; }
-
-        public int DoctorId { get; set; }
-        public Doctor Doctor { get; set; }
-
-        public DateTime StartAt { get; set; }
-        public DateTime EndAt { get; set; }
-        public string Notes { get; set; }
     }
 }
