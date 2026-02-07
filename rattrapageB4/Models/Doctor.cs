87fca9a062aac0ab781d1480771513703acbf872
hugob <hugob@LAPTOP-V6M8LCR8>
2026-02-07 19:41:16 +0100

    correction crud : -> pas de doublons    -> pas de chiffre dans nom/prenom

--- rattrapageB4/Models/Doctor.cs
+++ rattrapageB4/Models/Doctor.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace rattrapageB4.Models
 {
@@ -6,7 +7,11 @@ namespace rattrapageB4.Models
     {
         public int Id { get; set; }
         public string FirstName { get; set; }
+        [Required]
+        [MaxLength(15)]
         public string LastName { get; set; }
+        [Required]
+        [MaxLength(15)]
 
         public int SpecialityId { get; set; }
         public Speciality Speciality { get; set; }
